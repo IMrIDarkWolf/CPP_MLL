acf69efa64584f653dd8ba0ccc5219ccaa69543a
Mr. DarkWolf <jaeki94@gmail.com>
2019-02-22 03:36:21 +0200

    added perseptron.h

---
+++ include/perseptron.h
@@ -0,0 +1,14 @@
+//
+// Created by mr_darkwolf on 16.02.19.
+//
+
+#ifndef IRIS_CLASSIFICATION_PERSEPTRON_H
+#define IRIS_CLASSIFICATION_PERSEPTRON_H
+
+namespace Net {
+    class Perseptron {
+        double learning_rate;
+    };
+}
+
+#endif //IRIS_CLASSIFICATION_PERSEPTRON_H
